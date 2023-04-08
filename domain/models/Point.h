26c844b15406b5e4269cce316913b57d0853c3fa
vladuhalys <vlad.lysenko1999@gmail.com>
2023-04-08 14:24:14 +0300

    Fill class Point

--- domain/models/Point.h
+++ domain/models/Point.h
@@ -1,1 +1,32 @@
-#pragma once
+#include "../../internal/application.h"
+
+class Point
+{
+private:
+		int x;
+		int y;
+public:
+	Point() = default;
+	Point(int x, int y): x(x), y(y) {}
+	int getX()
+	{
+		return x;
+	}
+	int getY()
+	{
+		return y;
+	}
+	void setX(int x)
+	{
+		this->x = x;
+	}
+	void setY(int y)
+	{
+		this->y = y;
+	}
+	void print()
+	{
+		cout << "[x: " << x << ", y: " << y << "]" << endl;
+	}
+	~Point() = default;
+};
